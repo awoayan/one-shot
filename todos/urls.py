95abfaf2e2b2166039bf04878a26bb2444302bcd
Ayan <foxtrotsin9@gmail.com>
2023-04-18 18:29:06 -0400

    Feature 9 Complete

--- todos/urls.py
+++ todos/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.show_list, name="todo_list_list"),
     path("<int:id>/", views.todo_list_detail, name="todo_list_detail"),
     path("<int:id>/edit/", views.todo_list_detail, name="todo_list_detail"),
+    path("create/", views.todo_list_create, name="todo_list_create"), 
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from todos.forms import TodoListForm
 from todos.models import TodoList
 # Create your views here.
+
 
 def show_list(request):
     model_list = TodoList.objects.all()
@@ -9,13 +11,23 @@ def show_list(request):
     }
     return render(request, "todos/list.html", context)
 
+
 def todo_list_detail(request, id):
     lists = get_object_or_404(TodoList, id=id)
     context = {
         "list_object": lists}
     return render(request, "todos/detail.html", context)
 
-# def todo_lists_update(request,id):
-#     list = get_object_or_404(TodoList, id=id)
-#     if request.method == "POST":
-#         update_list
+
+def todo_list_create(request):
+    if request.method == "POST" and TodoListForm:
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            list = form.save()
+            return redirect("todo_list_detail", id=list.id)
+    else:
+        form = TodoListForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "todos/create_list.html", context)
