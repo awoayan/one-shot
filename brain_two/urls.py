a04b1e3e28a28eed785579cac8e9d48d5381efcf
Ayan <foxtrotsin9@gmail.com>
2023-04-18 10:59:29 -0400

    Feature 2 Complete

--- brain_two/urls.py
+++ brain_two/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangoneshot project.
+URL configuration for brain_two project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- brain_two/wsgi.py
+++ brain_two/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoneshot project.
+WSGI config for brain_two project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoneshot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brain_two.settings')
 
 application = get_wsgi_application()
